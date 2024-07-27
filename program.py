1d1979fe3529748fa0217df25bebb0f86cddbfb4
Davi Leite <106539266+Davilt4@users.noreply.github.com>
2024-07-27 19:06:18 -0300

    Estilização dos widgets do frame 1

--- program.py
+++ program.py
@@ -15,11 +15,9 @@ class Application:
         self.root.configure(background='#222831')
         self.root.geometry('700x500')
         self.root.resizable(False, False)
-        self.root.maxsize(width=900, height=700)
-        self.root.minsize(width=500, height=300)
 
     def frames(self): 
-        self.frame_1 = Frame(self.root,bd=4,bg="#222831",highlightbackground="#EEEEEE",highlightthickness=1)
+        self.frame_1 = Frame(self.root,bd=4,bg="#222831",highlightbackground="#EEEEEE",highlightthickness=0)
         self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
 
         self.frame_2 = Frame(self.root,bd=4,bg="#31363F",highlightbackground="#EEEEEE",highlightthickness=1)
@@ -27,26 +25,44 @@ class Application:
 
     def widgets_frame_1(self):
         self.bt_limpar = Button(self.frame_1,text='Limpar',bg="#76ABAE")
-        self.bt_limpar.place(relx=0.2, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.bt_limpar.place(relx=0.2, rely=0.10, relwidth=0.1, relheight=0.15,)
 
         self.bt_buscar = Button(self.frame_1,text='Buscar',bg="#76ABAE")
-        self.bt_buscar.place(relx=0.3, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.bt_buscar.place(relx=0.8, rely=0.10, relwidth=0.1, relheight=0.15)
 
         self.bt_novo = Button(self.frame_1,text='Novo',bg="#76ABAE")
-        self.bt_novo.place(relx=0.6, rely=0.2, relwidth=0.1, relheight=0.15)
-
+        self.bt_novo.place(relx=0.5, rely=0.10, relwidth=0.1, relheight=0.15)
+    
         self.bt_alterar = Button(self.frame_1,text='Alterar',bg="#76ABAE")
-        self.bt_alterar.place(relx=0.7, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.bt_alterar.place(relx=0.6, rely=0.10, relwidth=0.1, relheight=0.15)
 
         self.bt_excluir = Button(self.frame_1,text='Excluir',bg="#76ABAE")
-        self.bt_excluir.place(relx=0.8, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.bt_excluir.place(relx=0.7, rely=0.10, relwidth=0.1, relheight=0.15)
 
-        self.lb_codigo = Label(self.frame_1,text='Código',bg="#31363F")
-        self.lb_codigo.place(relx=0.05, rely=0.05)
+        self.lb_codigo = Label(self.frame_1,text='Código',bg="#31363F",fg="#EEEEEE")
+        self.lb_codigo.place(relx=0.05, rely=0.01)
 
         self.codigo_entry = Entry(self.frame_1)
-        self.codigo_entry.place(relx=0.05, rely=0.2, relwidth=0.09)
-       
+        self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.09)
+
+        self.lb_nome = Label(self.frame_1,text='Nome',bg="#31363F",fg="#EEEEEE")
+        self.lb_nome.place(relx=0.05, rely=0.35)
+
+        self.nome_entry = Entry(self.frame_1)
+        self.nome_entry.place(relx=0.05, rely=0.50, relwidth=0.75)
+
+        self.lb_email = Label(self.frame_1,text='Email',bg="#31363F",fg="#EEEEEE")
+        self.lb_email.place(relx=0.05, rely=0.65)
+
+        self.email_entry = Entry(self.frame_1)
+        self.email_entry.place(relx=0.05, rely=0.80, relwidth=0.2)
+
+        self.lb_cidade = Label(self.frame_1,text='Cidade',bg="#31363F",fg="#EEEEEE")
+        self.lb_cidade.place(relx=0.3, rely=0.65)
+
+        self.cidade_entry = Entry(self.frame_1)
+        self.cidade_entry.place(relx=0.3, rely=0.80, relwidth=0.4)
+
 
 Application()
  
